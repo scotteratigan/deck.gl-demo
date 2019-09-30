419db1965bc5e269bae3ac87ec1c8dc6e722aff5
ScottE <scotteratigan@gmail.com>
2019-09-30 08:44:09 -0700

    Adding build to serve on gh-pages

---
+++ client/build/precache-manifest.f36508a55b98f23907dbe0fe55bc5b0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de541c317acc230da9d81dfbdac275c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f962bfffa8b8bd12777b",
+    "url": "/static/css/main.d682c340.chunk.css"
+  },
+  {
+    "revision": "20202d721cbc62dd48e7",
+    "url": "/static/js/2.724b2cb1.chunk.js"
+  },
+  {
+    "revision": "f962bfffa8b8bd12777b",
+    "url": "/static/js/main.5d7e0013.chunk.js"
+  },
+  {
+    "revision": "7f45875b99a5ef02142c",
+    "url": "/static/js/runtime-main.c2763ce9.js"
+  },
+  {
+    "revision": "a42ac48ee6174b37986a5fe4f57c1120",
+    "url": "/static/media/bus.a42ac48e.svg"
+  },
+  {
+    "revision": "142982352398aace9d045423d05e6934",
+    "url": "/static/media/drone.14298235.svg"
+  }
+]);
